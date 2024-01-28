5499e78e82301e0df6f050f3975ee5dddcdaae93
LumiMuri <kst.anyway@gmail.com>
2024-01-28 15:45:12 +0200

    created maximum2

--- LumiMuri/helloworld.js
+++ LumiMuri/helloworld.js
@@ -7,22 +7,36 @@ let s="Значение переменной а="
 s =s+a
 //console.log(s)
 
-let arr=[222222,-34,-666,-35,-2,-76,-488,-1]
+let arr=[222,-34,547,-35,-2,-76,488,-1]
 //console.log(arr.length)
 
 let arrl=arr.length
 //console.log(arrl)
 //console.log("3ий элемент массива="+arr[2])
 
-let max=arr[0]
-for(let i=0; i<arrl;i=i+1) {
+let max1=arr[0]
+//for(let i=0; i<arrl;i=i+1) {
     //console.log(arr[i])
-    if (arr[i+1]>max){
-        max=arr[i+1]
-    }
-}
-console.log("максимум массива="+max)
+    //if (arr[i+1]>max1){
+        //max1=arr[i+1]
+//}
+//console.log("максимум массива="+max)
 
+let max2=arr[1]
+if(max2>max1){max1=arr[1]
+max2=arr[0]}
+
+    for(let i=0; i<arrl;i=i+1) {
+    if (arr[i+1]>max1){
+        max2=max1
+        max1=arr[i+1]
+    }
+        if(arr[i+1]<max1&&arr[i+1]>max2) {
+            max2 = arr[i + 1]
+        }
+}
+console.log("максимум 1="+max1)
+console.log("максимум 2="+max2)
 
 
 
