aa8796ebaf8a494923dc70e5c83334e90ee9a7ea
LumiMuri <kst.anyway@gmail.com>
2024-01-28 21:12:50 +0200

    find2max

---
+++ LumiMuri/findmax.js
@@ -0,0 +1,54 @@
+console.log(Number.MIN_VALUE)
+
+ let arr=[222,222,547,488,-2,547,488,-1]
+//let arr=[56]
+//console.log(arr.length)
+
+let arrl=arr.length
+//console.log("3ий элемент массива="+arr[2])
+if (arrl===0) {
+    console.log("в массиве 0 элементов")
+} else {
+    let max1 = arr[0]
+    if (arrl === 1) {
+        console.log("максимум массива = " + max1)
+    } else {
+        let max2
+        if (arrl > 1) {
+            if (arr[0] > arr[1]) {
+                max2 = arr[1]
+            }
+            if (arr[1] > arr[0]) {
+                max1 = arr[1]
+                max2 = arr[0]
+            }
+
+
+            for (let i = 0; i < arrl; i = i + 1) {
+                if (arr[i + 1] > max1) {
+                    max2 = max1
+                    max1 = arr[i + 1]
+                }
+                if (arr[i + 1] < max1) {
+                    max2 = arr[i + 1]
+                }
+            }
+            for (let i = 0; i < arrl; i = i + 1) {
+                if (arr[i + 1] > max1) {
+                    max2 = max1
+                    max1 = arr[i + 1]
+                }
+                if (arr[i + 1] < max1 && arr[i + 1] > max2) {
+                    max2 = arr[i + 1]
+                }
+            }
+        }
+        if (max1 === max2) {
+            console.log("максимум массива =" + max1)
+        }
+        if (max1 !== max2) {
+            console.log("максимум 1 = " + max1)
+            console.log("максимум 2 = " + max2)
+        }
+    }
+}
--- LumiMuri/helloworld.js
+++ LumiMuri/helloworld.js
@@ -3,17 +3,21 @@ let a=33
 //console.log(a)
 //console.log("Значение переменной а="+a)
 
+console.log(Number.MIN_VALUE)
+
 let s="Значение переменной а="
 s =s+a
 //console.log(s)
 
-let arr=[222,-34,547,-35,-2,-76,488,-1]
+let arr=[222,222,547,488,-2,547,488,-1]
 //console.log(arr.length)
 
 let arrl=arr.length
 //console.log(arrl)
 //console.log("3ий элемент массива="+arr[2])
-
+if (arrl===0) {
+    console.log("в массиве 0 элементов")
+}
 let max1=arr[0]
 //for(let i=0; i<arrl;i=i+1) {
     //console.log(arr[i])
@@ -21,22 +25,48 @@ let max1=arr[0]
         //max1=arr[i+1]
 //}
 //console.log("максимум массива="+max)
-
-let max2=arr[1]
-if(max2>max1){max1=arr[1]
-max2=arr[0]}
-
-    for(let i=0; i<arrl;i=i+1) {
-    if (arr[i+1]>max1){
-        max2=max1
-        max1=arr[i+1]
+if(arrl===1) {
+    console.log("максимум массива="+max1)
+}
+let max2
+if(arrl>1) {
+    if (arr[0] > arr[1]) {
+        max2 = arr[1]
     }
-        if(arr[i+1]<max1&&arr[i+1]>max2) {
+    if (arr[1] > arr[0]) {
+        max1 = arr[1]
+        max2 = arr[0]
+    }
+
+
+    for (let i = 0; i < arrl; i = i + 1) {
+        if (arr[i + 1] > max1) {
+            max2 = max1
+            max1 = arr[i + 1]
+        }
+        if (arr[i + 1] < max1) {
             max2 = arr[i + 1]
         }
+    }
+    for (let i = 0; i < arrl; i = i + 1) {
+        if (arr[i + 1] > max1) {
+            max2 = max1
+            max1 = arr[i + 1]
+        }
+        if (arr[i + 1] < max1 && arr[i + 1] > max2) {
+            max2 = arr[i + 1]
+        }
+    }
 }
-console.log("максимум 1="+max1)
-console.log("максимум 2="+max2)
+if(max1===max2) {
+    console.log("максимум массива ="+max1)
+}
+if(max1!==max2) {
+    console.log("максимум 1 = "+max1)
+    console.log("максимум 2 = "+max2)
+}
+
+
 
 
 
